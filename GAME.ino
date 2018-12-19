2045f41a1635c0d6f8b0aa72b0c6fe8f08508648
ardunioforschool <39209963+ardunioforschool@users.noreply.github.com>
2018-12-19 20:17:58 +1300

    Add files via upload

---
+++ GAME.ino
@@ -0,0 +1,36 @@
+int sensor = 4;
+int buzzer = 5;
+int LEDR = 6;
+int LEDG = 7;
+int reset = 9;
+
+void setup() {
+  Serial.begin(9600);
+  digitalWrite(reset, LOW);
+  pinMode(sensor, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(reset, OUTPUT);
+}
+
+void loop() {
+  Serial.println(digitalRead(4));
+  delay(100);
+  if (digitalRead(4) == LOW)
+  {
+  digitalWrite(LEDG, HIGH);
+  delay(100);
+  digitalWrite(LEDG, LOW);
+  }
+  if (digitalRead(4) == HIGH)
+  {
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(LEDR, HIGH); 
+    delay(100);
+    digitalWrite(LEDR, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(reset, HIGH);
+  }
+}
+
